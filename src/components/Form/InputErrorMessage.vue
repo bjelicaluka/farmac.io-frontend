8f533a4f6e5296a6879ee12755321acb86480258
Panic <panic.milos99@gmail.com>
2021-03-20 15:07:38 +0100

    feat: implemented select option input

---
+++ src/components/Form/InputErrorMessage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-if="!isValid" align="center">
+        <p class="text-danger mb-0">
+            <small>
+                <slot />
+            </small>
+        </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['isValid']
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -18,12 +18,25 @@
                 </div>
               </div>
             </form-row>
-            <div class="col-12">
+            <form-row>
+              <div class="col-12">
               <div class="form-group">
                 <label class="bmd-label-floating">Email address</label>
                 <input type="email" class="form-control">
               </div>
             </div>
+            </form-row>
+            <form-row>
+              <SelectOptionInput
+                label="Select you favourite food"
+                :isValid="isValid"
+                :onChange="onChange"
+                :options="options"
+                :showErrorMessage="showErrorMessage"
+                errorMessage="That is not food"
+              >
+              </SelectOptionInput>
+            </form-row>
           </form-group>
           <button class="btn btn-primary pull-right" type="submit"> Test </button>
         </Form>
@@ -35,19 +48,20 @@
     <Button
       type="button"
       className="btn btn-info"
-      :onClick="onClick"
+      :onClick="toggleError"
     >
-      Click me!
+      Toggle showErrorMessage! {{showErrorMessage}}
     </Button>
 
     <ButtonWithIcon
       type="button"
       className="btn btn-primary btn-round"
       iconName="favorite"
-      :onClick="onClick"
+      :onClick="toggleIsValid"
     >
-      Favourite me!
+      Toggle isValid! {{isValid}}
     </ButtonWithIcon>
+
 
   </div>
 </template>
@@ -59,8 +73,32 @@ import FormGroup from '../components/Form/FormGroup.vue';
 import FormRow from '../components/Form/FormRow.vue'
 import Button from '../components/Form/Button.vue'
 import ButtonWithIcon from '../components/Form/ButtonWithIcon.vue'
+import SelectOptionInput from '../components/Form/SelectOptionInput.vue'
+
+let selectOptions = [
+  {
+    value: 1,
+    label: 'Palacinke'
+  },
+  {
+    value: 2,
+    label: 'Rostilj iz leskovac'
+  },
+  {
+    value: 3,
+    label: 'Pica'
+  }
+];
 
 export default {
+  data: function() {
+    return {
+      isValid: false,
+      showErrorMessage: false,
+      options: selectOptions,
+      value: ''
+    }
+  },
   name: 'Home',
   components: {
     Form,
@@ -68,16 +106,27 @@ export default {
     FormRow,
     FormGroup,
     Button,
-    ButtonWithIcon
-},
+    ButtonWithIcon,
+    SelectOptionInput
+  },
   methods: {
     submitTest(e) {
       console.log(e);
     },
 
-    onClick(e) {
+    toggleError(e) {
       e.preventDefault();
-      console.log('hi');
+      this.showErrorMessage = !this.showErrorMessage;
+    },
+
+    toggleIsValid(e) {
+      e.preventDefault();
+      this.isValid = !this.isValid;
+    },
+
+    onChange(e) {
+      this.value = e.target.value;
+      console.log(this.value);
     }
   }
 }
