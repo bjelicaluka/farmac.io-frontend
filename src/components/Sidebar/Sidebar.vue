fd369be562ced56c7b3b6fa18ae095e9972ef40e
albertmakan <makanalbert@gmail.com>
2021-03-27 18:18:42 +0100

    fix: const role values in Sidebar

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="sidebar" data-color="green" data-background-color="white" :role="role">
+  <div class="sidebar" data-color="purple" data-background-color="white" :role="role">
       <sidebar-logo></sidebar-logo>
       <div class="sidebar-wrapper">
-        <ul class="nav" v-if="role==='Pharmacist'">
-          <sidebar-item name="Dashboard" icon="dashboard" to="#"></sidebar-item>
+        <ul class="nav" v-if="role===Roles.Pharmacist">
+          <sidebar-item name="Dashboard" icon="dashboard" to="/"></sidebar-item>
           <sidebar-item name="My patients" icon="contacts" to="/patients_list"></sidebar-item>
           <sidebar-item name="Consultation report" icon="assignment" to="/report"></sidebar-item>
           <sidebar-item name="Work calendar" icon="event" to="/work_calendar"></sidebar-item>
@@ -11,8 +11,8 @@
           <sidebar-item name="New appointment" icon="book_online" to="/new_appointment"></sidebar-item>
           <sidebar-item name="Issue medicine" icon="medication" to="/issue_medicine"></sidebar-item>
         </ul>
-        <ul class="nav" v-else-if="role==='Dermatologist'">
-          <sidebar-item name="Dashboard" icon="dashboard" to="#"></sidebar-item>
+        <ul class="nav" v-else-if="role===Roles.Dermatologist">
+          <sidebar-item name="Dashboard" icon="dashboard" to="/"></sidebar-item>
           <sidebar-item name="My patients" icon="contacts" to="/patients_list"></sidebar-item>
           <sidebar-item name="Consultation report" icon="assignment" to="/report"></sidebar-item>
           <sidebar-item name="Work calendar" icon="event" to="/work_calendar"></sidebar-item>
@@ -20,7 +20,7 @@
           <sidebar-item name="New appointment" icon="book_online" to="/new_appointment"></sidebar-item>
         </ul>
         <ul class="nav" v-else>
-          <sidebar-item name="Dashboard" icon="dashboard" to="#"></sidebar-item>
+          <sidebar-item name="Dashboard" icon="dashboard" to="/"></sidebar-item>
         </ul>
       </div>
     </div>
@@ -29,17 +29,23 @@
 <script>
 import SidebarItem from './SidebarItem.vue'
 import SidebarLogo from './SidebarLogo.vue'
+import Roles from '../../constants.js'
 
 export default {
+  data: () => {
+    return {
+      Roles,
+    }
+  },
   components: {
     SidebarLogo,
     SidebarItem
   },
   props: {
-        role: {
-            type: String,
-            default: ''
-        }
+    role: {
+        type: String,
+        default: ''
+    }
   }
 }
 </script>
