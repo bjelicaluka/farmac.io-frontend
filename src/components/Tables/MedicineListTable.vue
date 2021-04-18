cd36cdddccd6b3b1afda05f597d4fed263a86567
Luka Bjelica <bjelicaluka99@gmail.com>
2021-04-18 18:13:06 +0200

    feat: implemented list of medicines that are in stock for pharmacy

---
+++ src/components/Tables/MedicineListTable.vue
@@ -0,0 +1,29 @@
+<template>
+    <Table>
+        <TableHead :columnNames="['Medicine name', 'Quantity', 'Average Grade']"></TableHead>
+        <TableBody>
+            <TableRow 
+            v-for="(pharmacyMedicine, i) in medicines" 
+            :key="i" 
+            :values="[pharmacyMedicine.medicine.name, pharmacyMedicine.quantity, pharmacyMedicine.medicine.averageGrade]" />
+        </TableBody>
+    </Table>
+</template>
+
+<script>
+
+import Table from '../Table/Table.vue'
+import TableHead from '../Table/TableHead.vue'
+import TableBody from '../Table/TableBody.vue'
+import TableRow from '../Table/TableRow.vue'
+
+export default {
+    props: ['medicines'],
+    components: {
+        Table,
+        TableHead,
+        TableBody,
+        TableRow
+    },
+}
+</script>
