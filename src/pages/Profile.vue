ad783b678ba77ac7fada27dfedddc359174f05fc
jojev <jo.jevtic999@gmail.com>
2021-04-19 02:33:43 +0200

    feat: displayed negative points.

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -14,6 +14,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="#allergies" data-toggle="tab"><i class="material-icons">local_pharmacy</i>Allergies</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#negativePoints" data-toggle="tab"><i class="material-icons">warning_amber</i>Negative points</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -33,6 +36,16 @@
                             <AllergiesForm/>
                         </div>
                     </div>
+                    <div class="tab-pane" id="negativePoints">
+                        <div class=" justify-content-center align-items-center">
+                            <br />
+                            <br />
+                            <h3 v-if="patient">You have {{ patient.user.negativePoints }} negative points.</h3>
+                            <br />
+                            <p>If you have 3 or more negative points you cannot reserve medicines and book appointments with a dermatologist and pharmacist.
+                                You get negative points if you do not take the reserved medicine or do not appear at the examination or counseling."</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -44,12 +57,37 @@
 import UpdateProfileForm from '../components/Forms/UpdateProfileForm'
 import ChangePasswordForm from '../components/Forms/ChangePasswordForm'
 import AllergiesForm from '../components/Forms/AllergiesForm'
+import {mapActions, mapGetters} from 'vuex'
+import { getAccountIdFromToken } from '../utils/token'
 
 export default {
     components: {
         ChangePasswordForm,
         AllergiesForm,
         UpdateProfileForm
+    },
+    data: function(){
+        return {
+            patient: null
+        }
+    },
+    computed: {
+        ...mapGetters({
+            getPatient: 'patient/getPatient'
+        })
+    },
+    watch: {
+        getPatient(patient){
+            this.patient = patient;
+        }
+    },
+    methods: {
+        ...mapActions({
+            fetchPatient: 'patient/fetchPatientById'
+        })
+    },
+    mounted() {
+        this.fetchPatient(getAccountIdFromToken());
     }
 }
 </script>
